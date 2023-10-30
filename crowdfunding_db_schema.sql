52fe31afbb9876b04df4523a0b55c9b036bb0951
spribojac <sophie170995@live.co.uk>
2023-10-30 22:26:01 +0000

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -19,18 +19,18 @@ CREATE TABLE "Subcategory" (
 );
 
 CREATE TABLE "Campaign" (
-    "cf_id" SERIAL   NOT NULL,
+    "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR(255)   NOT NULL,
-    "description" TEXT   NOT NULL,
-    "goal" NUMERIC   NOT NULL,
-    "pledged" NUMERIC   NOT NULL,
+    "description" VARCHAR(255)  NOT NULL,
+    "goal" INT,
+    "pledged" INT,
     "outcome" VARCHAR(255)   NOT NULL,
     "backers_count" INT   NOT NULL,
     "country" VARCHAR(255)   NOT NULL,
     "currency" VARCHAR(10)   NOT NULL,
-    "launch_date" TIMESTAMP   NOT NULL,
-    "end_date" TIMESTAMP   NOT NULL,
+    "launch_date" TIMESTAMP NOT NULL,
+    "end_date" TIMESTAMP NOT NULL,
     "category_id" VARCHAR(10)   NOT NULL,
     "subcategory_id" VARCHAR(10)   NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY (
